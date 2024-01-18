a2d03447081a646b7251674e8b9372568bd31102
AlishaWilfred <alisha3105henry@gmail.com>
2024-01-18 15:42:40 +0530

    expo setup

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,22 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     plugins: [
+//       // Required for expo-router
+//       'expo-router/babel',
+//     ],
+//   };
+// };
+
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      // Required for expo-router
-      'expo-router/babel',
+      "nativewind/babel",
+      require.resolve("expo-router/babel")
     ],
   };
 };
